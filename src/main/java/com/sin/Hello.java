40255f550a5a156a08e42ba218f9fe09b07952cf
你的姓名(英文亦可) <Email>
2020-02-02 21:37:09 +0800

    variable, data  type

--- src/main/java/com/sin/Hello.java
+++ src/main/java/com/sin/Hello.java
@@ -6,6 +6,18 @@ public class Hello {
         //new Person().helo();
         Person p=new Person();
         p.helo();
+        int age = 23;
+        Integer age2=19;
+        char c = '我';
+        Character c2='A';
+        byte b=120;
+        float weight= 66.5f;
+        float  height =1.5f;
+        boolean adult =true;
+
+
+        String name="Joker";
+
 
 
     }
--- src/main/kotlin/com/sin/Hello.kt
+++ src/main/kotlin/com/sin/Hello.kt
@@ -5,6 +5,14 @@ fun main(args: Array<String>) {
     //Human().hello()
     val h = Human()
     h.hello()
+    var age =23;
+    age=24;
+    var weight=66.5f;
+    var name:String
+    name="Joker"
+
+
+
 
 }
 
